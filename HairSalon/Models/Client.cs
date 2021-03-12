1a69df0332a5ee9dd7902ecac9bd2ac8a5ac9bdb
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-12 09:21:33 -0800

    Finish creating classes and HairSalonContext

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,9 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public string Name { get; set; }
+    public int ClientId { get; set; }
+    public int StylistId { get; set; }
+    public Stylist Stylist { get; set; }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,6 +4,15 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
-        
+      public string Name { get; set; }
+      public string Style { get; set; }
+      public string Schedule { get; set; }
+      public int StylistId { get; set; }
+      public virtual ICollection<Client> Clients { get; set; }
+
+      public Stylist()
+      {
+        this.Clients = new HashSet<Client>();
+      }
     }
 }

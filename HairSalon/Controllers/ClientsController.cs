55215756c2f5ad74cce145653b5a88bf56fe50e3
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-12 13:59:05 -0800

    Build Create view for clients, second create view WIP

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+  }
+}

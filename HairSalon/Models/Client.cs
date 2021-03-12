fc771132cb5cbcec914abb4f8476d7470e3510d5
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-12 10:34:51 -0800

    Add virtual to client property, create index view for stylists

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -5,6 +5,6 @@ namespace HairSalon.Models
     public string Name { get; set; }
     public int ClientId { get; set; }
     public int StylistId { get; set; }
-    public Stylist Stylist { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }

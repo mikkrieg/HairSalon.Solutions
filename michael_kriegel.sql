e0e62a5e3816263bcd8fc2efe66313429605b5dc
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-12 15:55:34 -0800

    Edit layout, rename database within .sql file

--- michael_kriegel.sql
+++ michael_kriegel.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
 --
--- Host: localhost    Database: mikey_kriegel
+-- Host: localhost    Database: michael_kriegel
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
